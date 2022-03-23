99739a818fabeec6957e58b65d8cf46ad2a3f11a
venkat6597 <82959004+venkat6597@users.noreply.github.com>
2022-03-24 00:00:06 +0530

    Update TestGreeter.java

--- Sample_CICD_Build&Release/server/src/test/java/com/example/TestGreeter.java
+++ Sample_CICD_Build&Release/server/src/test/java/com/example/TestGreeter.java
@@ -19,14 +19,14 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "ww";
+    String someone = "12";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
+    String someone = "12";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
